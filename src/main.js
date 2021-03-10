9ac2a60a0477f5edb6b37a47ca157cbe2683b984
jeannawong <jeannawong25@gmail.com>
2021-03-09 21:26:36 -0600

    Updated student Profiles route

--- src/main.js
+++ src/main.js
@@ -5,15 +5,16 @@ import axios from "axios";
 
 // Configures axios library for signup
 axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development"
+    ? "https://whispering-sands-76473.herokuapp.com/api/students"
+    : "http://localhost:3000";
 
 // This adds the jwt token for authorization
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
   axios.defaults.headers.common["Authorization"] = "Bearer " + jwt;
 }
-
-
 
 createApp(App)
   .use(router)
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -8,22 +8,22 @@
         </li>
       </ul>
       <div class="form-group">
-        <label>Name:</label> 
-        <input type="text" class="form-control" v-model="name">
+        <label>Name:</label>
+        <input type="text" class="form-control" v-model="name" />
       </div>
       <div class="form-group">
         <label>Email:</label>
-        <input type="email" class="form-control" v-model="email">
+        <input type="email" class="form-control" v-model="email" />
       </div>
       <div class="form-group">
         <label>Password:</label>
-        <input type="password" class="form-control" v-model="password">
+        <input type="password" class="form-control" v-model="password" />
       </div>
       <div class="form-group">
         <label>Password confirmation:</label>
-        <input type="password" class="form-control" v-model="passwordConfirmation">
+        <input type="password" class="form-control" v-model="passwordConfirmation" />
       </div>
-      <input type="submit" class="btn btn-primary" value="Submit">
+      <input type="submit" class="btn btn-primary" value="Submit" />
     </form>
   </div>
 </template>
@@ -38,7 +38,7 @@ export default {
       email: "",
       password: "",
       passwordConfirmation: "",
-      errors: []
+      errors: [],
     };
   },
   methods: {
@@ -47,19 +47,18 @@ export default {
         name: this.name,
         email: this.email,
         password: this.password,
-        password_confirmation: this.passwordConfirmation
+        password_confirmation: this.passwordConfirmation,
       };
       axios
         .post("/api/students", params)
         .then(response => {
           console.log(response.data);
           this.$router.push("/login");
-       
         })
         .catch(error => {
           this.errors = error.response.data.errors;
         });
-    }
-  }
+    },
+  },
 };
 </script>
