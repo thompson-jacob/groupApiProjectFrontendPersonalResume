5f6578b5c5e9d8ab75001b94535ee75436bc2422
thompson-jacob <63691359+thompson-jacob@users.noreply.github.com>
2021-03-10 18:02:41 -0500

    added rest of profile data to template from DB, added experience etc to data

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="profile">
-    <p>{{ student }}</p>
-    <p>{{ student.last_name }}</p>
+    <div class="container">
+    <p> Your Profile </p>
+    <p>{{ student[0].first_name }} {{ student[0].last_name }}</p>
+    <p>{{ student[0].email }} </p>
+    <p>{{ student[0].phone_number }} </p>
+    <p> {{ student[0].short_bio }} </p>
+    <p> {{ student[0].linkedin_url }} </p>
+    <p> {{ student[0].twitter_handle }} </p>
+    <p> {{ student[0].website_url }} </p>
+    <p> {{ student[0].github_url }} </p>
+    <p> {{ student[0].resume_url }} </p>
+    <button>edit basic info</button>
+   </div>
+    <!-- <p> {{ student[0]["experiences"] }} </p> -->
   </div>
 </template>
 
@@ -10,7 +22,9 @@
 <script>
 import axios from "axios";
 
+
 export default {
+  
   data: function() {
     return {
       student: {
@@ -25,7 +39,33 @@ export default {
         resume_url: "",
         github_url: "",
         photo: "",
+
+        experiences: {
+          start_date: "",
+          end_date: "",
+          job_title: "",
+          company_name: "",
+          details: "",
+        },
+        projects: {
+          name: "",
+          description: "",
+          url: "",
+          screenshot:"",
+        },
+        skills: {
+          skill_name:""
+        },
+        educations: {
+          start_date: "",
+          end_date: "",
+          degree: "",
+          university_name: "",
+          details: "",
+        },
+
       },
+      
     };
   },
   created: function() {
@@ -33,10 +73,11 @@ export default {
   },
   methods: {
     showProfile: function() {
-      axios.get("/").then(response => {
+      axios.get("/api/students").then(response => {
         console.log(response.data);
         this.student = response.data;
       });
+    
     },
   },
 };
