327497585b0b6969dbd7e7ca22f935df92f4f8ec
thompson-jacob <63691359+thompson-jacob@users.noreply.github.com>
2021-03-10 18:51:40 -0500

    non working patch request & data

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -12,6 +12,58 @@
     <p> {{ student[0].github_url }} </p>
     <p> {{ student[0].resume_url }} </p>
     <button>edit basic info</button>
+    <div class="edit-basic">
+    <form v-on:submit.prevent="updateProfile()">
+      <h1>Edit Profile</h1>
+      <ul>
+        <li class="text-danger" v-for="error in errors" v-bind:key="error">
+          {{ error }}
+        </li>
+      </ul>
+      
+      <div class="form-group">
+        <label>First Name:</label>
+        <input type="text" class="form-control" v-model="first_name">
+      </div>
+      <div class="form-group">
+        <label>Last name:</label>
+        <input type="text" class="form-control" v-model="last_name">
+      </div>
+      <div class="form-group">
+        <label>Email:</label>
+        <input type="text" class="form-control" v-model="email">
+      </div>
+      <div class="form-group">
+        <label>Phone:</label>
+        <input type="text" class="form-control" v-model="phone_number">
+      </div>
+      <div class="form-group">
+        <label>Bio:</label>
+        <input type="text" class="form-control" v-model="short_bio">
+      </div>
+      <div class="form-group">
+        <label>LinkedIn URL:</label>
+        <input type="text" class="form-control" v-model="linkedin_url">
+      </div>
+      <div class="form-group">
+        <label>Twitter:</label>
+        <input type="text" class="form-control" v-model="twitter_handle">
+      </div>
+      <div class="form-group">
+        <label>GitHub:</label>
+        <input type="text" class="form-control" v-model="github_url">
+      </div>
+      <div class="form-group">
+        <label>Personal Website:</label>
+        <input type="text" class="form-control" v-model="website_url">
+      </div>
+      <div class="form-group">
+        <label>Resume:</label>
+        <input type="text" class="form-control" v-model="resume_url">
+      </div>
+      <input type="submit" class="btn btn-primary" value="Submit">
+    </form>
+  </div>
    </div>
     <!-- <p> {{ student[0]["experiences"] }} </p> -->
   </div>
@@ -77,7 +129,30 @@ export default {
         console.log(response.data);
         this.student = response.data;
       });
-    
+    },
+    updatePost: function() {
+      var params = {
+        first_name: this.first_name,
+        last_name: this.last_name,
+        email: this.email,
+        phone_number: this.phone_number,
+        short_bio: this.short_bio,
+        linkedin_url: this.linkedin_url,
+        twitter_handle: this.twitter_handle,
+        website_url: this.website_url,
+        resume_url: this.resume_url,
+        github_url: this.github_url,
+        // photo: this.photo,
+      };
+      axios
+        .patch("/api/students/" + this.$route.params.id, params)
+        .then(response => {
+          console.log(response.data);
+          this.$router.push("/students" + this.$route.params.id);
+        })
+        .catch(error => {
+          this.errors = error.response.data.errors;
+        });
     },
   },
 };
