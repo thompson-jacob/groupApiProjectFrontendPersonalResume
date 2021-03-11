c9950f712240723f22355acb4b6108dd796b045b
jeannawong <jeannawong25@gmail.com>
2021-03-10 18:31:17 -0600

    Updated Signup page

--- src/main.js
+++ src/main.js
@@ -6,9 +6,7 @@ import axios from "axios";
 // Configures axios library for signup
 axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development"
-    ? "https://whispering-sands-76473.herokuapp.com"
-    : "http://localhost:3000";
+  process.env.NODE_ENV === "development" ? "https://whispering-sands-76473.herokuapp.com" : "http://localhost:3000";
 
 // This adds the jwt token for authorization
 var jwt = localStorage.getItem("jwt");
